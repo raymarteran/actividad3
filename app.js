b2684bd6f6e29bed28eff60728d9ec1dd4b23451
raymarteran <teran.raymar1@gmail.com>
2025-03-24 15:16:55 -0400

    creado el html para la pantalla de inicio de sesion

--- app.js
+++ app.js
@@ -36,9 +36,9 @@ app.set('views', path.join(__dirname, 'views'));
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
-
+// res.sendFile(path.join(__dirname, 'public', 'index.html'));
 app.get('/usersVista', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'users.html'));
 });
